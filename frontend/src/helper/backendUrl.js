b9d963711b14aae0fd620265526d523564d819f3
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-02-04 13:49:31 +0530

    backend domain change

--- frontend/src/helper/backendUrl.js
+++ frontend/src/helper/backendUrl.js
@@ -1,3 +1,3 @@
 const isDev = window.location.hostname === "localhost"
 
-export const BACKEND_URL = isDev ? "http://localhost:3000/api/" : "https://domainsapi.shivanshup.xyz/"
+export const BACKEND_URL = isDev ? "http://localhost:3000/api/" : "https://domains-api.shivanshup.xyz/"
