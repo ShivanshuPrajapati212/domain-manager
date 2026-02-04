25e37fa3ffe300320075d34997d46584988bf0b0
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-02-04 20:09:47 +0530

    add recursive mongo connect

--- backend/config/mongo.conf.js
+++ backend/config/mongo.conf.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 
+
+i = 0
+
 const connectToMongo = () => {
-  try {
-    mongoose.connect(process.env.MONGO_URI)
-      .then(() => {
-        console.log("Connected to MongoDB");
-      }).catch(() => {
-        console.log("Failed to connect in MongoDB");
-      });
-  } catch (error) {
-    console.log("Error:", error);
-  }
+    try {
+        mongoose.connect(process.env.MONGO_URI)
+            .then(() => {
+                console.log("Connected to MongoDB");
+            }).catch(() => {
+                console.log("Failed to connect in MongoDB");
+            });
+    } catch (error) {
+        console.log("Error:", error);
+        if (i < 5) {
+            connectToMongo()
+        }
+        i++
+    }
 };
 
 module.exports = connectToMongo;
